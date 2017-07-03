e4fcd8c28516f0147e9be2fbbf240bd3f9d71262
Don <donjpacheco@gmail.com>
2017-07-04 05:40:27 +0800

    Add working API for news crawling connected to scrapy

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dataengr
+
+
+
+
+
+Set the following as your environment variables to run the system
+MONGODB_SERVER
+MONGODB_USERNAME
+MONGODB_PASSWORD
+DEBUG
+SECRET_KEY
+ALLOWED_HOSTS
--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework_mongoengine.viewsets import ModelViewSet as MongoModelViewSet
+
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy import log, signals
+from crawler.spiders.news import NewsSpider
+from scrapy.utils.project import get_project_settings
 
 from serializers import NewsItemSerializer
 from models import NewsItem
@@ -23,3 +32,35 @@ class NewsItemSet(MongoModelViewSet):
         if keyword:
             return queryset.search_text(keyword)
         return queryset
+
+
+
+class ScrapyCrawlSet(viewsets.ViewSet):
+    """
+    Request type: GET
+    Start Scrapy News Crawler
+
+    Headers:
+    Language: en_US
+
+    Response:
+
+    """
+    __name__ = 'ScrapyCrawl'
+
+    def list(self, request):
+        """
+        Run Scrapy crawl
+        :param request:
+        :param format:
+        :return:
+        """
+
+        spider = NewsSpider(domain='bbc.com')
+        settings = get_project_settings()
+        runner = CrawlerRunner(settings)
+        runner.crawl(spider)
+        if reactor.running is False:
+            reactor.run()
+            
+        return Response(status=status.HTTP_200_OK)
